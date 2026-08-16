ad021ba9cef72fa3512391acaeaa3268c10d7214
lucian <161787746+LuongCaoChanHuyIT@users.noreply.github.com>
2026-08-16 20:33:42 +0700

    feat: integrate shadcn/ui components and configure Tailwind CSS styles

--- app/routes/dashboard-page.tsx
+++ app/routes/dashboard-page.tsx
@@ -1,3 +1,13 @@
+import { ArrowUpIcon } from "lucide-react";
+import { Button } from "~/components/ui/button";
+
 export default function DashboardPage() {
-  return <div>hello</div>;
+  return (
+    <div className="flex flex-wrap items-center gap-2 p-6">
+      <Button variant="outline">Button</Button>
+      <Button variant="outline" size="icon" aria-label="Submit">
+        <ArrowUpIcon />
+      </Button>
+    </div>
+  );
 }
